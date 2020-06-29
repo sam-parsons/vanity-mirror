0f3dbfa71743c16aca969c0fdc7eef16e6d222a3
Sam Parsons <samparsons269@gmail.com>
2020-06-28 23:51:19 -0400

    initial commit

--- Vanity Mirror/AppDelegate.swift
+++ Vanity Mirror/AppDelegate.swift
@@ -7,20 +7,75 @@
 //
 
 import Cocoa
+import SwiftUI
+import HotKey
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var popover: NSPopover!
+    
+    var statusBarItem: NSStatusItem!
+    
+    // toggles camera pane
+    @objc func togglePopover(_ sender: AnyObject?) {
+         if let button = self.statusBarItem.button {
+              if self.popover.isShown {
+                   self.popover.performClose(sender)
+              } else {
+                   self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+              }
+         }
+    }
+    
+    private var hotKey: HotKey? {
+        didSet {
+            guard let hotKey = hotKey else {
+                print("HotKey not available")
+                return
+            }
+            
+            hotKey.keyDownHandler = { [weak self] in
+                self?.togglePopover(self)
+            }
+        }
+    }
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        // register the hotkey
+        register(self)
+        
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 400)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        if let button = self.statusBarItem.button {
+             button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+             button.action = #selector(togglePopover(_:))
+        }
     }
+    
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        // unregister hotkey
+        unregister(self)
+    }
+    
+    @IBAction func unregister(_ sender: Any?) {
+        hotKey = nil
     }
 
+    @IBAction func register(_ sender: Any?) {
+        hotKey = HotKey(keyCombo: KeyCombo(key: .v, modifiers: [.command, .option]))
+    }
 
 }
 
---
+++ Vanity Mirror/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vanity Mirror
+//
+//  Created by Sam Parsons on 6/28/20.
+//  Copyright © 2020 Sam Parsons. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
