047c6a9ea1c6670a0590d5ad1de86d4a6c7d68f4
Sam Parsons <samparsons269@gmail.com>
2020-06-28 20:24:48 -0400

    Initial Commit

---
+++ Vanity Mirror/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Vanity Mirror
+//
+//  Created by Sam Parsons on 6/28/20.
+//  Copyright © 2020 Sam Parsons. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Vanity Mirror/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Vanity Mirror
+//
+//  Created by Sam Parsons on 6/28/20.
+//  Copyright © 2020 Sam Parsons. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
